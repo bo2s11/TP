ced13c8038d602b5e69f77ea39021b8ad8139c22
ebrahima <ebrahima11@hotmail.fr>
2018-04-19 14:56:29 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""
+# Exo 1
+
+text = "je suis du texte"
+
+print(text * 3)
+
+# Exo 2
+
+i = 1
+while i < 1000:
+	print (i)
+	i = i + 2
+i = 0
+
+# Exo 3
+
+tab = 13
+i = 0
+
+while i < 10:
+    print (i * tab)
+    i = i + 1
+
+# exo 4
+
+mot = input ("give me a word ")
+
+print(len(mot))
+
+# exo 5
+mot = int(input ("Give me a number "))
+if (mot % 2 == 0):
+    print ("pair")
+else:
+    print ("impair")
+"""
